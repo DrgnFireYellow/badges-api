e31e0f4ee75c6d548c77bc59f8cc4213f1d6dde6
DrgnFireYellow <ethan@milbert.dev>
2022-04-14 12:38:19 -0400

    escape input

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // IMPORTS
 const express = require("express");
+escape = require("lodash.escape")
 // CONSTANTS
 const app = express();
 const port = 8080;
@@ -34,9 +35,9 @@ app.get("/languages/go", (req, res) => {
 });
 // USERS
 app.get("/user/", (req, res) => {
-  username = req.query.username;
-  color = req.query.color;
-  usertype = req.query.type;
+  username = escape(req.query.username);
+  color = escape(req.query.color);
+  usertype = escape(req.query.type);
   res.send(
     `http://img.shields.io/badge/-${username}-${color}?logo=${usertype}&style=for-the-badge`
   );
